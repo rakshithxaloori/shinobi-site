da61fc50fb5bd942d3c5f76b5b592418eec55487
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-11-16 13:07:35 +0530

    add screenshot

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import PrivacyPolicy from "./components/privacy_policy";
 import Terms from "./components/terms";
 import Delete from "./components/delete";
 
+import FeedScreenshot from "./screenshots/feed.png";
+
 function App() {
   return (
     <Router>
@@ -41,17 +43,27 @@ function App() {
 function Home() {
   return (
     <div className="Home">
-      <div>
-        <p className="Title">Welcome to Shinobi!</p>
-        <p className="Subtitle">
-          League of Legends' match history and champion masteries!
-        </p>
-        <p className="Subtitle">
-          Stalk, follow, chat and share matches with your friends!
-        </p>
+      <div className="Non-Screenhots">
+        <div>
+          <p className="Title">Welcome to Shinobi!</p>
+          <p className="Subtitle">
+            League of Legends' match history and champion masteries!
+          </p>
+          <p className="Subtitle">
+            Stalk, follow, chat and share matches with your friends!
+          </p>
+        </div>
+        <div className="Store-icons">
+          <PlayStore />
+        </div>
       </div>
-      <div className="Store-icons">
-        <PlayStore />
+      <div className="Screenshots">
+        <img
+          src={FeedScreenshot}
+          height={600}
+          className="Header-logo"
+          alt="Feed Screen"
+        />
       </div>
     </div>
   );
