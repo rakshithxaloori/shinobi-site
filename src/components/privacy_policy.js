9f30a57cab99b0164d299a33139704ec334ec6e5
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2021-12-27 11:36:27 +0530

    add clips usage to policy

--- src/components/privacy_policy.js
+++ src/components/privacy_policy.js
@@ -7,46 +7,58 @@ import MailTo from "./mailto";
 const PrivacyPolicy = () => (
   <div className="privacy-policy">
     <div className="Text-body">
-      <strong>Privacy Policy</strong>{" "}
+      <strong>Privacy Policy</strong>
       <p>
         Shinobi built the Shinobi app as a Freemium app. This SERVICE is
         provided by Shinobi at no cost and is intended for use as is.
-      </p>{" "}
+      </p>
       <p>
         This page is used to inform visitors regarding our policies with the
         collection, use, and disclosure of Personal Information if anyone
         decided to use our Service.
-      </p>{" "}
+      </p>
       <p>
         If you choose to use our Service, then you agree to the collection and
         use of information in relation to this policy. The Personal Information
         that we collect is used for providing and improving the Service. We will
         not use or share your information with anyone except as described in
         this Privacy Policy.
-      </p>{" "}
+      </p>
       <p>
         The terms used in this Privacy Policy have the same meanings as in our
         Terms and Conditions, which is accessible at Shinobi unless otherwise
         defined in this Privacy Policy.
-      </p>{" "}
+      </p>
+      <p>
+        <strong>App Usage</strong>
+      </p>
+      <p>
+        The content - clips - uploaded by you have to be video-gaming clips
+        only. If you find content that is not so, please report. Violation could
+        lead to account suspension or ban.
+      </p>
+      <p>
+        The clips cannot contain sexually explicit content, threats, harassment,
+        or bullying, particularly toward minors.
+      </p>
       <p>
         <strong>Information Collection and Use</strong>
-      </p>{" "}
+      </p>
       <p>
         For a better experience, while using our Service, we may require you to
         provide us with certain personally identifiable information, including
         but not limited to email address. The information that we request will
         be retained by us and used as described in this privacy policy.
-      </p>{" "}
+      </p>
       <div>
         <p>
           The app does use third party services that may collect information
           used to identify you.
-        </p>{" "}
+        </p>
         <p>
           Link to privacy policy of third party service providers used by the
           app
-        </p>{" "}
+        </p>
         <ul>
           <li className="Privacy-policy-list-item">
             <a
@@ -89,10 +101,10 @@ const PrivacyPolicy = () => (
             </a>
           </li>
         </ul>
-      </div>{" "}
+      </div>
       <p>
         <strong>Log Data</strong>
-      </p>{" "}
+      </p>
       <p>
         We want to inform you that whenever you use our Service, in a case of an
         error in the app we collect data and information (through third party
@@ -101,15 +113,15 @@ const PrivacyPolicy = () => (
         name, operating system version, the configuration of the app when
         utilizing our Service, the time and date of your use of the Service, and
         other statistics.
-      </p>{" "}
+      </p>
       <p>
         <strong>Cookies</strong>
-      </p>{" "}
+      </p>
       <p>
         Cookies are files with a small amount of data that are commonly used as
         anonymous unique identifiers. These are sent to your browser from the
         websites that you visit and are stored on your device's internal memory.
-      </p>{" "}
+      </p>
       <p>
         This Service does not use these “cookies” explicitly. However, the app
         may use third party code and libraries that use “cookies” to collect
@@ -117,39 +129,39 @@ const PrivacyPolicy = () => (
         accept or refuse these cookies and know when a cookie is being sent to
         your device. If you choose to refuse our cookies, you may not be able to
         use some portions of this Service.
-      </p>{" "}
+      </p>
       <p>
         <strong>Service Providers</strong>
-      </p>{" "}
+      </p>
       <p>
         We may employ third-party companies and individuals due to the following
         reasons:
-      </p>{" "}
+      </p>
       <ul>
-        <li>To facilitate our Service;</li>{" "}
-        <li>To provide the Service on our behalf;</li>{" "}
-        <li>To perform Service-related services; or</li>{" "}
+        <li>To facilitate our Service;</li>
+        <li>To provide the Service on our behalf;</li>
+        <li>To perform Service-related services; or</li>
         <li>To assist us in analyzing how our Service is used.</li>
-      </ul>{" "}
+      </ul>
       <p>
         We want to inform users of this Service that these third parties have
         access to your Personal Information. The reason is to perform the tasks
         assigned to them on our behalf. However, they are obligated not to
         disclose or use the information for any other purpose.
-      </p>{" "}
+      </p>
       <p>
         <strong>Security</strong>
-      </p>{" "}
+      </p>
       <p>
         We value your trust in providing us your Personal Information, thus we
         are striving to use commercially acceptable means of protecting it. But
         remember that no method of transmission over the internet, or method of
         electronic storage is 100% secure and reliable, and we cannot guarantee
         its absolute security.
-      </p>{" "}
+      </p>
       <p>
         <strong>Links to Other Sites</strong>
-      </p>{" "}
+      </p>
       <p>
         This Service may contain links to other sites. If you click on a
         third-party link, you will be directed to that site. Note that these
@@ -157,10 +169,10 @@ const PrivacyPolicy = () => (
         to review the Privacy Policy of these websites. We have no control over
         and assume no responsibility for the content, privacy policies, or
         practices of any third-party sites or services.
-      </p>{" "}
+      </p>
       <p>
         <strong>Children’s Privacy</strong>
-      </p>{" "}
+      </p>
       <p>
         These Services do not address anyone under the age of 13. We do not
         knowingly collect personally identifiable information from children
@@ -169,23 +181,23 @@ const PrivacyPolicy = () => (
         our servers. If you are a parent or guardian and you are aware that your
         child has provided us with personal information, please contact us so
         that we will be able to do necessary actions.
-      </p>{" "}
+      </p>
       <p>
         <strong>Changes to This Privacy Policy</strong>
-      </p>{" "}
+      </p>
       <p>
         We may update our Privacy Policy from time to time. Thus, you are
         advised to review this page periodically for any changes. We will notify
         you of any changes by posting the new Privacy Policy on this page.
-      </p>{" "}
-      <p>This policy is effective as of 2021-10-11</p>{" "}
+      </p>
+      <p>This policy is effective as of 2021-10-11</p>
       <p>
         <strong>Contact Us</strong>
-      </p>{" "}
+      </p>
       <p>
         If you have any questions or suggestions about our Privacy Policy, do
         not hesitate to contact us at <MailTo />.
-      </p>{" "}
+      </p>
     </div>
   </div>
 );
